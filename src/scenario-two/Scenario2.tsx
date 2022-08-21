7f13a7a9a3d9047b79730f8331c2fce14239a2b7
felix.velazquez@solera.com <felix.velazquez@solera.com>
2022-08-21 22:50:46 +0200

    UI is created dinamycally depending on a predefined taskList

--- src/scenario-two/Scenario2.tsx
+++ src/scenario-two/Scenario2.tsx
@@ -3,15 +3,41 @@ import Button from '@mui/material/Button';
 import { Card, Checkbox, FormControlLabel, Grid, IconButton, TextField, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
+import { useState } from 'react';
+
+type Task = {
+    text: string;
+    done: boolean;
+  };
 
 const Scenario2 = () => {
+
+    let initialTaskList: Task[] = 
+        [ 
+            {text: "Apply for GBG Senior Fullstack developer", done: true},
+            {text: "Send resignation letter", done: false},             
+            {text: "Do a good job", done: false}             
+        ]
+
+    const [taskList, setTaskList] = useState(initialTaskList);
+
+    let numPendingTasks = 0;
+
+    // eslint-disable-next-line array-callback-return
+    taskList.map((task: Task) => {
+        if (!task.done)
+        {
+            numPendingTasks++;
+        }
+    }); 
+
     return <>
             <p>
                 Scenario 2 page
             </p>
     
-                <Card variant="outlined">
-                    <Grid container justifyContent="center" >
+                <Card variant="outlined"  style={{marginBottom:40, paddingBottom:10}}>
+                    <Grid container justifyContent="center" style={{marginBottom:10}} >
                         <Grid item>
                             <Typography variant="h4">
                                 Tasks
@@ -21,51 +47,45 @@ const Scenario2 = () => {
 
                     <Grid item>
                         <TextField id="outlined-basic" label="Add your new To Do" variant="outlined" />
-                        <Button variant="contained">+</Button>
+                        <Button variant="contained" style={{marginLeft:10, height:55}}>
+                            <Typography style={{fontSize:36}}>
+                                +
+                            </Typography>
+                        </Button>
                     </Grid>
 
                     <Grid item>
                         <Grid container justifyContent="center" >
+                            {taskList.map((task) =>                             
                                 <Grid container justifyContent="center" >
                                     <Grid item>
                                         <FormControlLabel
-                                            value="This is the first"
-                                            control={<Checkbox />}
-                                            label="Apply for GBG Senior Fullstack developer"
+                                            value={task.text}
+                                            control={<Checkbox checked={task.done} />}
+                                            label={task.text}
                                             labelPlacement="end"
+                                            style={{textDecorationLine: task.done? 'line-through' : 'none'}}
                                             />     
-                                        <IconButton aria-label="done" color="primary">
+                                        <IconButton aria-label="done" disabled={task.done} color="primary">
                                             <CreateIcon />
                                         </IconButton>                                               
                                         <IconButton aria-label="delete">
                                             <DeleteIcon />
                                         </IconButton>                
                                     </Grid>
-                                </Grid>
-                                <Grid container justifyContent="center" >
-                                    <Grid item>
-                                        <FormControlLabel
-                                            value="This is the second"
-                                            control={<Checkbox />}
-                                            label="Send resignation letter"
-                                            labelPlacement="end"
-                                            />         
-                                    <IconButton aria-label="done" color="primary">
-                                            <CreateIcon />
-                                        </IconButton>                                          
-                                    <IconButton aria-label="delete" color="primary">
-                                            <DeleteIcon />
-                                        </IconButton>                                                  
-                                    </Grid>
-                            </Grid>
+                                </Grid>                            
+                                )
+                            }                         
                         </Grid>
                     </Grid>                    
 
                     <Grid item>
-                        <Typography>
-                            You have 3 pending tasks"
-                        </Typography>
-                        <Button variant="outlined" color="warning">Clear all</Button>
+                        <Grid container justifyContent="center" style={{marginTop:10}}>
+                            <Typography>
+                                You have {numPendingTasks} pending tasks
+                            </Typography>
+                            <Button variant="outlined" color="warning" style={{marginLeft:10}}>Clear all</Button>
+                        </Grid>
                     </Grid>
                 </Card>
            </>
